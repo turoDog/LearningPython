f769e9cabeb72f7e251184fc22eb54096a4d94d0
turod <turoDog@foxmail.com>
2018-06-23 02:03:16 +0800

    第一个pygame游戏

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,35 @@
+import sys
+
+import pygame
+
+from settings import Settings
+
+from ship import Ship
+
+import game_functions as gf
+
+def run_game():
+	#初始化游戏并创建一个屏幕对象
+	#初始化pygame、设置和屏幕对象
+	pygame.init()
+	ai_settings = Settings()
+	screen = pygame.display.set_mode(
+		(ai_settings.screen_width, ai_settings.screen_height))
+	# screen = pygame.display.set_mode((1200,800))
+	pygame.display.set_caption("Alien Invasion")
+
+	#创建一艘飞船
+	ship = Ship(screen)
+
+	#设置背景色
+	# bg_color = (230,230,230)
+
+	#开始游戏的主循环
+	while True:
+
+		#监视键盘和鼠标事件
+		gf.check_events()
+
+		gf.update_screen(ai_settings, screen, ship)
+
+run_game()
