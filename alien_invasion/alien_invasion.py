c16b5b12f729a5bc745ac842b58ce87a704e0e85
turod <turoDog@foxmail.com>
2018-07-03 20:54:49 +0800

    添加子弹、控制射击、限制子弹数量、添加外星人

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -8,6 +8,8 @@ from ship import Ship
 
 import game_functions as gf
 
+from pygame.sprite import Group
+
 def run_game():
 	#初始化游戏并创建一个屏幕对象
 	#初始化pygame、设置和屏幕对象
@@ -21,6 +23,9 @@ def run_game():
 	#创建一艘飞船
 	ship = Ship(ai_settings,screen)
 
+	#创建一个用于存储子弹的编组
+	bullets = Group()
+
 	#设置背景色
 	# bg_color = (230,230,230)
 
@@ -28,10 +33,12 @@ def run_game():
 	while True:
 
 		#监视键盘和鼠标事件
-		gf.check_events(ship)
+		gf.check_events(ai_settings, screen, ship, bullets)
 
 		ship.update()
 
-		gf.update_screen(ai_settings, screen, ship)
+		gf.update_bullets(bullets)
+
+		gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -5,8 +5,8 @@ class Settings():
 		"""初始化游戏的设置"""
 		#屏幕设置
 		self.screen_width = 1200
-		self.screen_height = 800
-		self.bg_color = (0,0,255)
+		self.screen_height = 750
+		self.bg_color = (230,230,230)
 
 		# 飞船的设置
 		self.ship_speed_factor = 1.5
@@ -16,4 +16,5 @@ class Settings():
 		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = 60, 60, 60
+		self.bullets_allowed = 3
 		
