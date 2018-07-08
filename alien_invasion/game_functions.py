5a9ae56df652599f9d837bc6faebeb903ecdf453
turod <turoDog@foxmail.com>
2018-07-08 11:15:25 +0800

    生成新的外星人群

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -60,7 +60,7 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
 	#让最近绘制的屏幕可见
 	pygame.display.flip()
 
-def update_bullets(aliens, bullets):
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
 	"""更新子弹的位置，并删除已消失的子弹"""
 	# 更新子弹的位置
 	bullets.update()
@@ -74,6 +74,11 @@ def update_bullets(aliens, bullets):
 	# 检查是否有子弹击中了外星人
 	# 如果是这样，就删除相应的子弹与外星人
 	collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+	if len(aliens) == 0:
+		# 删除现有的子弹并新建一群外星人
+		bullets.empty()
+		create_fleet(ai_settings, screen, ship, aliens)
 
 def fire_bullet(ai_settings, screen, ship, bullets):
 	"""如果还没有到达限制，就发射一颗子弹"""
