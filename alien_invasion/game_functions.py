5704276751a7c0f825d2a6cfc681bbf02f2008c8
turod <turoDog@foxmail.com>
2018-07-10 22:46:15 +0800

    重置速度

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -31,6 +31,9 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
 	"""在玩家单击Play按钮时开始新游戏"""
 	button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
 	if button_clicked and not stats.game_active:
+		# 重置游戏设置
+		ai_settings.initialize_dynamic_settings()
+
 		# 隐藏光标
 		pygame.mouse.set_visible(False)
 
