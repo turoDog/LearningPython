d14d183bc97c7ef91e5688abcee1ad5b32386c5a
turod <turoDog@foxmail.com>
2018-07-10 22:43:03 +0800

    修改速度设置

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -2,7 +2,7 @@ class Settings():
 	"""存储《外星人入侵》的所有设置的类"""
 
 	def __init__(self):
-		"""初始化游戏的设置"""
+		"""初始化游戏的静态设置"""
 		#屏幕设置
 		self.screen_width = 1200
 		self.screen_height = 750
@@ -23,5 +23,25 @@ class Settings():
 		self.alien_speed_factor = 1
 		self.fleet_drop_speed = 10
 		# fleet_direction为1表示向右移，为 -1表示向左移
-		self.fleet_direction = 1
+		# self.fleet_direction = 1
 		
+		# 以什么样的速度加快游戏节奏
+		self.speedup_scale = 1.1
+
+		self.initialize_dynamic_settings()
+
+	def initialize_dynamic_settings(self):
+		"""初始化随游戏进行而变化的设置"""
+		self.ship_speed_factor = 1.5
+		self.bullet_speed_factor = 3
+		self.alien_speed_factor = 1
+
+		# fleet_direction为1表示向右移，为 -1表示向左移
+		self.fleet_direction = 1
+
+	def increase_speed(self):
+		"""提高速度设置"""
+		self.ship_speed_factor *= self.speedup_scale
+		self.bullet_speed_factor *= self.speedup_scale
+		self.alien_speed_factor *= self.speedup_scale
+			
