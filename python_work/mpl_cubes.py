3f8464136b57e11ffea070bf5a70f53e8654f3c8
turod <turoDog@foxmail.com>
2018-07-29 18:39:42 +0800

    绘制前五个整数立方值的折线图

---
+++ python_work/mpl_cubes.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+# 绘制前五个整数的散点图
+
+x_values = [1,2,3,4,5]
+cubes = [x**3 for x in x_values]
+
+# 设置图表标题，并给坐标轴加上标签
+plt.title("Cube Numbers", fontsize = 24)
+plt.xlabel("Value", fontsize = 14)
+plt.ylabel("Cube of Value", fontsize = 14)
+
+# 设置刻度标记的大小
+plt.tick_params(axis='both', labelsize = 14)
+
+plt.plot(x_values, cubes, linewidth=5)
+
+plt.show()
