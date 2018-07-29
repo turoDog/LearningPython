6d17a697fba1515d71b3217149864c18f4fec034
turod <turoDog@foxmail.com>
2018-07-29 17:28:38 +0800

    校正图形

--- python_work/mpl_squares.py
+++ python_work/mpl_squares.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 
 # 绘制简单的折线图
+input_values = [1,2,3,4,5,6,7,8,9,10,11]
 squares = [1,4,9,16,25,36,49,64,81,100,121]
-plt.plot(squares, linewidth=5)
+# plt.plot(squares, linewidth=5)
+plt.plot(input_values, squares, linewidth=5)
 
 # 设置图表标题，并给坐标轴加上标签
 plt.title("Square Numbers", fontsize = 24)
@@ -11,8 +13,6 @@ plt.ylabel("Squares of Value", fontsize = 14)
 
 # 设置刻度标记的大小
 plt.tick_params(axis='both', labelsize = 14)
-
-plt.plot(squares)
 
 plt.show()
 
