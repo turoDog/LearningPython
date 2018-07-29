a6e4f0f750934633f92e748b1b9d2517df8b9486
turod <turoDog@foxmail.com>
2018-07-29 18:04:58 +0800

    自动计算数据

--- python_work/scatter_squares.py
+++ python_work/scatter_squares.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
 
-x_values = [1,2,3,4,5]
-y_values = [1,4,9,16,25]
-plt.scatter(x_values, y_values, s = 100)
+# x_values = [1,2,3,4,5]
+# y_values = [1,4,9,16,25]
+# plt.scatter(x_values, y_values, s = 100)
+
+x_values = list(range(1,1001))
+y_values = [x**2 for x in x_values]
+
+plt.scatter(x_values, y_values, s=40)
 
 # plt.scatter(2, 4, s = 200)
 
@@ -13,5 +18,8 @@ plt.ylabel("Squares of Value", fontsize=14)
 
 # 设置刻度标记的大小
 plt.tick_params(axis = "both", which = "major", labelsize = 14)
+
+# 设置每个坐标轴的取值范围
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
