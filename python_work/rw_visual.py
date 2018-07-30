d61765543c6bc8e61bf88c9cea70f6c788fdea3f
turod <turoDog@foxmail.com>
2018-07-30 22:28:05 +0800

    隐藏坐标轴

--- python_work/rw_visual.py
+++ python_work/rw_visual.py
@@ -18,6 +18,10 @@ while True:
 	plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
 		s=100)
 
+	# 隐藏坐标轴
+	plt.axes().get_xaxis().set_visible(False)
+	plt.axes().get_yaxis().set_visible(False)
+
 	plt.show()
 
 	keep_running = input("Make another walk? (y/n): ")
