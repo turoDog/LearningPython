9806bff9f4847addb63ec9babc6b3bd9c585572b
turod <turoDog@foxmail.com>
2018-08-04 17:47:10 +0800

    打印文件头及其位置，提取并读取数据

--- python_work/chapter16/csv/highs_lows.py
+++ python_work/chapter16/csv/highs_lows.py
@@ -1,7 +1,14 @@
 import csv
 
+# 从文件中获取最高气温
 filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
 	reader = csv.reader(f)
 	header_row = next(reader)
-	print(header_row)
+
+	highs = []
+	for row in reader:
+		high = int(row[1])
+		highs.append(high)
+
+	print(highs)
