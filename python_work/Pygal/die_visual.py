df1d17f1060f9e467ebac4af3b0104fb5d538de1
turod <turoDog@foxmail.com>
2018-08-04 15:16:40 +0800

    绘制直方图

--- python_work/Pygal/die_visual.py
+++ python_work/Pygal/die_visual.py
@@ -1,4 +1,5 @@
 from die import Die
+import pygal
 
 # 创建一个 D6
 die = Die()
@@ -15,4 +16,13 @@ for value in range(1, die.num_sides+1):
 	frequency = results.count(value)
 	frequencies.append(frequency)
 
-print(frequencies)
+# 对结果进行可视化
+hist = pygal.Bar()
+
+hist.title = "Results of rolling onr D6 1000 times."
+hist.x_labels = ['1','2','3','4','5','6']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
