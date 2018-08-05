cc1069a6c8c88bcbc2943e4e5bf6b0e94fa156cf
turod <turoDog@foxmail.com>
2018-08-05 19:48:28 +0800

    处理API响应

---
+++ python_work/chapter17/python_repos.py
@@ -0,0 +1,12 @@
+import requests
+
+# 执行API调用并存储响应
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print('Status code:', r.status_code)
+
+# 将API响应存储在一个变量中
+response_dict = r.json()
+
+# 处理结果
+print(response_dict.keys())
