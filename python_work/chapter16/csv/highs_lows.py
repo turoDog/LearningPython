9651bf821935c786af2aea1439a75f1595882fb5
turod <turoDog@foxmail.com>
2018-08-05 16:37:28 +0800

    错误检查

--- python_work/chapter16/csv/highs_lows.py
+++ python_work/chapter16/csv/highs_lows.py
@@ -5,21 +5,25 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 
 # 从文件中获取日期，最高气温和最低气温
-filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
 	reader = csv.reader(f)
 	header_row = next(reader)
 
 	dates, highs, lows = [], [], []
 	for row in reader:
-		current_date = datetime.strptime(row[0], "%Y-%m-%d")
-		dates.append(current_date)
-
-		high = int(row[1])
-		highs.append(high)
-
-		low = int(row[3])
-		lows.append(low)
+		try:
+			current_date = datetime.strptime(row[0], "%Y-%m-%d")
+			high = int(row[1])
+			low = int(row[3])
+		except ValueError:
+			print(current_date, 'missing data')
+		else:
+			dates.append(current_date)
+			highs.append(high)
+			lows.append(low)
+		
+		
 
 	# 根据数据绘制图形
 	fig = plt.figure(dpi=128, figsize=(10, 6))
@@ -28,7 +32,7 @@ with open(filename) as f:
 	plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 	# 设置图形的格式
-	plt.title("Daily high and low temperatures - 2014", fontsize=24)
+	plt.title("Daily high and low temperatures - 2014\nDeath Valley, CA", fontsize=20)
 	plt.xlabel('', fontsize=16)
 	fig.autofmt_xdate()
 	plt.ylabel("Temperature (F)", fontsize=16)
