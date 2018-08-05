a99e2957c3a1c2bc35523166c96803daeac99e17
turod <turoDog@foxmail.com>
2018-08-05 21:47:35 +0800

    概述最受欢迎的仓库

--- python_work/chapter17/python_repos.py
+++ python_work/chapter17/python_repos.py
@@ -19,11 +19,13 @@ repo_dict = repo_dicts[0]
 # for key in sorted(repo_dict.keys()):
 # 	print(key)
 
-print("\nSelected information about first repository:")
-print('Name:', repo_dict['name'])
-print('Owner:', repo_dict['owner']['login'])
-print('Stars:', repo_dict['stargazers_count'])
-print('Repository:', repo_dict['html_url'])
-print('Created:', repo_dict['created_at'])
-print('Updated:', repo_dict['updated_at'])
-print('Description:', repo_dict['description'])
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+	print('Name:', repo_dict['name'])
+	print('Owner:', repo_dict['owner']['login'])
+	print('Stars:', repo_dict['stargazers_count'])
+	print('Repository:', repo_dict['html_url'])
+	print('Created:', repo_dict['created_at'])
+	print('Updated:', repo_dict['updated_at'])
+	print('Description:', repo_dict['description'])
+	print('\n')
