180cca7cc155b597477bf3f1c96f8f33c35f914f
turod <turoDog@foxmail.com>
2018-08-05 17:35:04 +0800

    获取两个字母的国别码

--- python_work/chapter16/Json/world_population.py
+++ python_work/chapter16/Json/world_population.py
@@ -1,5 +1,7 @@
 import json
 
+from country_codes import get_country_code
+
 # 将数据加载到一个列表中
 filename = 'population_data.json'
 with open(filename) as f:
@@ -10,4 +12,8 @@ for pop_dict in pop_data:
 	if pop_dict['Year'] == '2010':
 		country_name = pop_dict['Country Name']
 		population = int(float(pop_dict['Value']))
-		print(country_name + ":" + str(population))
+		code = get_country_code(country_name)
+		if code:
+			print(code + ": "+ str(population))
+		else:
+			print('Error - ' + country_name)
