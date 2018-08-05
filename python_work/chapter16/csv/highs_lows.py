7670eb173e116f4390d92674c03495713e066d44
turod <turoDog@foxmail.com>
2018-08-05 16:22:39 +0800

    给图标1区域着色

--- python_work/chapter16/csv/highs_lows.py
+++ python_work/chapter16/csv/highs_lows.py
@@ -23,8 +23,9 @@ with open(filename) as f:
 
 	# 根据数据绘制图形
 	fig = plt.figure(dpi=128, figsize=(10, 6))
-	plt.plot(dates, highs, c='red')
-	plt.plot(dates, lows, c='blue')
+	plt.plot(dates, highs, c='red', alpha=0.5)
+	plt.plot(dates, lows, c='blue', alpha=0.5)
+	plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 	# 设置图形的格式
 	plt.title("Daily high and low temperatures - 2014", fontsize=24)
