cdbd67e0b7f4d7364319d5f222d449ba4d4e2923
turod <turoDog@foxmail.com>
2018-08-05 21:41:10 +0800

    处理响应字典，了解 API 返回的 github 上最受欢迎的 Python 库

--- python_work/chapter17/python_repos.py
+++ python_work/chapter17/python_repos.py
@@ -7,6 +7,23 @@ print('Status code:', r.status_code)
 
 # 将API响应存储在一个变量中
 response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
 
-# 处理结果
-print(response_dict.keys())
+# 探索有关仓库的信息
+repo_dicts = response_dict['items']
+print("Repositories returned:", len(repo_dicts))
+
+# 研究第一个仓库
+repo_dict = repo_dicts[0]
+# print("\nKeys:", len(repo_dict))
+# for key in sorted(repo_dict.keys()):
+# 	print(key)
+
+print("\nSelected information about first repository:")
+print('Name:', repo_dict['name'])
+print('Owner:', repo_dict['owner']['login'])
+print('Stars:', repo_dict['stargazers_count'])
+print('Repository:', repo_dict['html_url'])
+print('Created:', repo_dict['created_at'])
+print('Updated:', repo_dict['updated_at'])
+print('Description:', repo_dict['description'])
