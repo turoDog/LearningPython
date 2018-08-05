6b76d9d60eff78d855b7bfc500483ed88837f8e9
turod <turoDog@foxmail.com>
2018-08-05 22:14:00 +0800

    改进 Pygal 图表

--- python_work/chapter17/python_repos.py
+++ python_work/chapter17/python_repos.py
@@ -37,7 +37,19 @@ for repo_dict in repo_dicts:
 
 # 可视化
 my_style = LS('#333366', base_style=LCS)
-chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+
+# 创建一个配置对象，它包含要传递给 Bar() 的左右定制
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on Github'
 chart.x_labels = names
 
