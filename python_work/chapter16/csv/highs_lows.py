8f7ae46a91264fc8b7af20c2cae4dd5b442f2285
turod <turoDog@foxmail.com>
2018-08-05 15:55:16 +0800

    绘制气温图表

--- python_work/chapter16/csv/highs_lows.py
+++ python_work/chapter16/csv/highs_lows.py
@@ -1,5 +1,7 @@
 import csv
 
+from matplotlib import pyplot as plt
+
 # 从文件中获取最高气温
 filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
@@ -10,5 +12,17 @@ with open(filename) as f:
 	for row in reader:
 		high = int(row[1])
 		highs.append(high)
+
+	# 根据数据绘制图形
+	fig = plt.figure(dpi=128, figsize=(10, 6))
+	plt.plot(highs, c='red')
+
+	# 设置图形的格式
+	plt.title("Daily high temperatures, July 2014", fontsize=24)
+	plt.xlabel('', fontsize=16)
+	plt.ylabel("Temperature (F)", fontsize=16)
+	plt.tick_params(axis='both', which='major', labelsize=16)
+
+	plt.show()
 
 	print(highs)
