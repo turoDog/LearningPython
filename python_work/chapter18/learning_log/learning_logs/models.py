8371d726ff0fd56e6470764c8fd0b8e50aac2616
turod <turoDog@foxmail.com>
2018-08-07 22:29:07 +0800

    定义模型 Entry

--- python_work/chapter18/learning_log/learning_logs/models.py
+++ python_work/chapter18/learning_log/learning_logs/models.py
@@ -13,7 +13,7 @@ class Topic(models.Model):
 
 class Entry(models.Model):
 	"""学到的有关某个主题的具体知识"""
-	topic = models.ForeignKey(Topic)
+	topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
 	text = models.TextField()
 	date_added = models.DateTimeField(auto_now_add=True)
 
