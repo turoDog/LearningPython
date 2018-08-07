4de02985fb1e00d942ad536eb4121118a71e09be
turod <turoDog@foxmail.com>
2018-08-07 22:23:00 +0800

    定义模型 Entry

--- python_work/chapter18/learning_log/learning_logs/models.py
+++ python_work/chapter18/learning_log/learning_logs/models.py
@@ -11,3 +11,18 @@ class Topic(models.Model):
 		"""返回模型的字符串表示"""
 		return self.text
 
+class Entry(models.Model):
+	"""学到的有关某个主题的具体知识"""
+	topic = models.ForeignKey(Topic)
+	text = models.TextField()
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		verbose_name_plural = 'entries'
+
+	def __str__(self):
+		"""返回对象模型的字符串表示"""
+		return self.text[:50] + "..."
+
+
+
