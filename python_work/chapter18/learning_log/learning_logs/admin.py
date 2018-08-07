2925099aef3fdc83f6e331814146197bbe660d62
turod <turoDog@foxmail.com>
2018-08-07 22:35:13 +0800

    向管理网站注册 Entry

--- python_work/chapter18/learning_log/learning_logs/admin.py
+++ python_work/chapter18/learning_log/learning_logs/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from learning_logs.models import Topic
+from learning_logs.models import Topic, Entry
 
 admin.site.register(Topic)
-
+admin.site.register(Entry)
 
