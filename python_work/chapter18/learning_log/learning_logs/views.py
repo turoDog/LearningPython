a39ff6c71eafca3829261ce9f4d3df6d61c46ca0
turod <turoDog@foxmail.com>
2018-08-12 22:39:45 +0800

    添加新条目--视图函数 new_entry

--- python_work/chapter18/learning_log/learning_logs/views.py
+++ python_work/chapter18/learning_log/learning_logs/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -37,4 +37,23 @@ def new_topic(request):
 			return HttpResponseRedirect(reverse('learning_logs:topics'))
 
 	context = {'form': form}
-	return render(request, 'learning_logs/new_topic.html', context)		
+	return render(request, 'learning_logs/new_topic.html', context)	
+
+def new_entry(request, topic_id):
+    """在特定主题中添加新条目"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # 未提交数据，创建一个空表单
+        form = EntryForm()
+    else:
+        # POST 提交的数据，对数据进行处理
+        form = EntryForm(data = request.POST)
+        if form.is_valid():
+        	new_entry = form.save(commit=False)
+        	new_entry.topic = topic
+        	new_entry.save()
+        	return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)    		
