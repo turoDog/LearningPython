800a7b3a431023df0231a5e3d603f03645a2c1f1
turod <turoDog@foxmail.com>
2018-08-12 22:37:53 +0800

    添加新条目--URL 模式 new_entry

--- python_work/chapter18/learning_log/learning_logs/urls.py
+++ python_work/chapter18/learning_log/learning_logs/urls.py
@@ -20,4 +20,7 @@ urlpatterns = [
 
 	# 用于添加新主题的网页
 	url(r'^new_topic/$', views.new_topic, name='new_topic'),
+
+	# 用于添加新条目的页面
+    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
 ]
