0d5a4ab16b510d5346eda09a7a0ed08603247870
turod <turoDog@foxmail.com>
2018-08-16 14:57:33 +0800

    编辑条目-- 视图函数edit_entry

--- python_work/chapter18/learning_log/learning_logs/views.py
+++ python_work/chapter18/learning_log/learning_logs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
@@ -56,4 +56,23 @@ def new_entry(request, topic_id):
         	return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
 
     context = {'topic': topic, 'form': form}
-    return render(request, 'learning_logs/new_entry.html', context)    		
+    return render(request, 'learning_logs/new_entry.html', context)    	
+
+def edit_entry(request, entry_id):
+	"""编辑既有条目"""
+	entry = Entry.objects.get(id = entry_id)
+	topic = entry.topic
+
+	if request.method != 'POST':
+		# 初次请求，使用当前条目填充表单
+		form = EntryForm(instance=entry)
+	else:
+		# POST 提交的数据，对数据进行处理
+		form = EntryForm(instance=entry, data=request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('learning_logs:topic',
+										args=[topic.id]))
+
+	context = {'entry':entry, 'topic':topic, 'form':form}
+	return render(request, 'learning_logs/edit_entry.html', context)    	
