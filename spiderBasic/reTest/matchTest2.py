cff026b7640288f962faef9d61bd093619c94d11
turod <turoDog@foxmail.com>
2018-09-22 15:22:49 +0800

    re 正则表达式 match ---- 贪婪与非贪婪

---
+++ spiderBasic/reTest/matchTest2.py
@@ -0,0 +1,8 @@
+import re
+
+content = 'Hello 123 4567 World_This is a Regex Demo'
+print(len(content))
+result = re.match('^Hello.*Demo$',content)
+print(result)
+print(result.group())
+print(result.span())
